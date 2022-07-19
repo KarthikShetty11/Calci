53ce9a1db8c5e03741b69f07b521f10839d5efc5
KarthikShetty11 <kvshetty27@gmail.com>
2022-07-19 13:16:58 +0530

    post method added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", (req,res)=>{
+    res.send("thanks for posting");
+})
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 })
